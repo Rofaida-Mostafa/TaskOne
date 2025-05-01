4b64a2478d036a99878e0fe1f1d7766c4ea5016f
Rofaida-Mostafa <rofaidaashraf3@gmail.com>
2025-05-01 14:17:30 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+# This stage is used when running from VS in fast mode (Default for Debug configuration)
+FROM mcr.microsoft.com/dotnet/runtime:9.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+
+# This stage is used to build the service project
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["Task_One/Task_One.csproj", "Task_One/"]
+RUN dotnet restore "./Task_One/Task_One.csproj"
+COPY . .
+WORKDIR "/src/Task_One"
+RUN dotnet build "./Task_One.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# This stage is used to publish the service project to be copied to the final stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./Task_One.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Task_One.dll"]
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+
+namespace Task_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//===========================================================
+            int smallCarpetPrice = 25;
+            int largeCarpetPrice = 35;
+            const double taxRate = 0.06;
+
+           Console.WriteLine(
+               "{" +
+               "List of price:\n " +
+               "Price per small room: $25, \n " +
+               "Price per large room: $35, \n " +
+               "Tax: $6.6, \n" +
+               " This estimate is valid for 30 days" +
+               "}\n" );
+
+            Console.WriteLine("What's the number of small carpets? ");
+            int? numOfSmallCarpet= Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("What's the number of small carpets? ");
+            int? numOfLargeCarpet= Convert.ToInt16(Console.ReadLine());
+            int? costBeforeTaxes = ((numOfSmallCarpet * smallCarpetPrice) + (numOfLargeCarpet * largeCarpetPrice));
+            double? costAfterTaxes = (costBeforeTaxes * taxRate) + costBeforeTaxes;
+
+            Console.WriteLine($"Number of small carpets: ${numOfSmallCarpet},\n" +
+                $"Number of large carpets: ${numOfLargeCarpet},\n" +
+                $"Price per small room: {(numOfSmallCarpet * smallCarpetPrice)}\n" +
+                $"Price per large room: {(numOfLargeCarpet * largeCarpetPrice)}\n" +
+                $"Total estimate:{costAfterTaxes}");
+
+//===========================================================
+
+            //   Islam's Carpet Cleaning Service
+            //    Charges:
+            //        $25 per small
+            //        $35 per large
+            //    Sales tax rate is 6 %
+            //    Estimates are valid for 30 days
+
+            //    Prompt the user for the number of small and large rooms they would like cleaned
+            //    and provide an estimate such as:
+            //Estimate for carpet cleaning service
+            //Number of small carpets: 3 < --
+            //Number of large carpets: 1 < --
+            //Price per small room: $25
+            //Price per large room: $35
+            //Cost : $110
+            //Tax: $6.6
+            //===============================
+            //Total estimate: $116.6
+            //This estimate is valid for 30 days
+
+            //hint: + - * /
+        }
+    }
+}
